bc53b0f11ef43ba83341c6e7d0ac85963e547d36
osenpb <oeperezbellido@gmail.com>
2025-11-12 20:52:13 -0500

    first commit

---
+++ src/main/java/com/dawi/sistema_solar_restapi/SistemaSolarRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.dawi.sistema_solar_restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaSolarRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaSolarRestapiApplication.class, args);
+	}
+
+}
